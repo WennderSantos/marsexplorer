6442abad988860be9c789ed2b21818f9ef8d4faa
Wennder dos Santos <wennder.santos@outlook.com>
2019-01-29 12:52:39 -0200

    Improve file-content->settings

--- src/marsexplorer/adapters.clj
+++ src/marsexplorer/adapters.clj
@@ -9,6 +9,10 @@
      :y         (Integer/parseInt (second line))
      :direction (keyword (last line))})
 
+(defn- line->instructions! [line]
+  (->> (map #(keyword (str %)) line)
+       (vec)))
+
 (defn- line->mars-length! [line {:keys [x y]}]
   {:pre [(s/valid? ::specs/mars-top-rigth-coord line)]}
   {:bottom-left {:x x
@@ -16,39 +20,23 @@
    :top-right   {:x (Integer/parseInt (first line))
                  :y (Integer/parseInt (second line))}})
 
-(defn- lines->explorers! [lines]
-  (loop [lines  lines
-         count  0
-         aux    {}
-         result []]
-    (cond
-      (empty? lines)
-        result
-      (= count 1)
-        (recur (rest lines)
-               0
-               {}
-               (conj result
-                     (assoc aux :instructions
-								                (vec
-                                  (map #(keyword (str %))
-									 		                 (first lines))))))
-      :else
-        (recur (rest lines)
-               (inc count)
-               (assoc aux :position
-                          (line->position! (-> (first lines)
-                                               (str/split #" "))))
-               result))))
-
 (defn file-content->settings! [mars-bottom-left-coord file-content]
-  (let [lines       (str/split-lines file-content)
-        mars-length (line->mars-length! (-> (first lines)
-                                            (str/split #" "))
-                                        mars-bottom-left-coord)
-        explorers   (lines->explorers! (rest lines))]
-    {:mars-length mars-length
-     :explorers   explorers}))
+  (let [lines (str/split-lines file-content)]
+    {:mars-length (line->mars-length! (-> (first lines)
+                                          (str/split #" "))
+                                          mars-bottom-left-coord)
+     :explorers (->> (rest lines)
+                     (partition 2)
+                     ((fn [lines-config]
+                       (map
+                         (fn [[line-position line-instructions]]
+                           (assoc {}
+                                  :position (-> line-position
+                                                (str/split #" ")
+                                                (line->position!))
+                                  :instructions (-> line-instructions
+                                                    (line->instructions!))))
+                          lines-config))))}))
 
 (defn position->cmdline-fmt [{:keys [x y direction]}]
   "Returns the values of a position separated by space"
